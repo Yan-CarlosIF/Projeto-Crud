86a9af90871f1c29ba580178e2c09a54a4b5c9c7
Yan Carlos <yan312005@gmail.com>
2025-02-24 14:24:19 -0300

    fix: dotenv in the root

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,12 +1,23 @@
 import "dotenv/config";
 import { Pool } from "pg";
 
+console.log(process.env.DB_URL);
+
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DB_URL, // Usando a string de conexão completa do NeonDB
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
+// Tentativa de conexão
+pool
+  .connect()
+  .then(() => console.log("🔥 Banco de dados conectado com sucesso!"))
+  .catch((err) =>
+    console.error("❌ Erro ao conectar ao banco de dados:", err.message)
+  );
+
+// Função para executar consultas
 export const query = (text: string, params?: any[]): Promise<any> =>
   pool.query(text, params);
